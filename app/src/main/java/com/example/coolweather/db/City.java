360dbfe84e39d69a09c58d70289af14161515539
MagiaClay <944295628@qq.com>
2019-10-19 13:01:55 +0800

    加入创建的数据库和表的各项配置

---
+++ app/src/main/java/com/example/coolweather/db/City.java
@@ -0,0 +1,42 @@
+package com.example.coolweather.db;
+
+import org.litepal.crud.LitePalSupport;
+
+public class City extends LitePalSupport {
+    private int id;
+    private  String cityName;
+    private int cityCode;
+    private int provincedId;//省ID
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvincedId() {
+        return provincedId;
+    }
+
+    public void setProvincedId(int provincedId) {
+        this.provincedId = provincedId;
+    }
+}
